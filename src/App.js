b51ecb0433844ebcec12de655dbe810a6a2c5f53
Matthew <matthewagordon123@gmail.com>
2023-05-01 03:45:34 -0400

    react

--- src/App.js
+++ src/App.js
@@ -7,25 +7,14 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import { ResourcesPage } from './pages/ResourcesPage';
 import { HomePage } from './pages/HomePage';
-import { FAQPage } from './pages/FAQPage';
-
-import { ProjectNavbar } from './components/ProjectNavbar';
 
 function App() {
   return (
     <Router>
-      <ProjectNavbar />
-    <div className="container" style={{marginTop: '35px'}}>
-      <div className="row">
-        <div className="col-12">
       <Routes>
         <Route exact path="/" element={<HomePage/>} />
-        <Route path="/resources" element={<ResourcesPage/>} />
-        <Route path="/faqs" element={<FAQPage/>} />
       </Routes>
-    </div></div></div>
     </Router>
   );
 }
